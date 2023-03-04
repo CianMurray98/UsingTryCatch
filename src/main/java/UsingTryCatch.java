0d05c1f8c967c3688e1fc442dae6b8591177a4a9
cian murray <g00358011@atu.ie>
2023-03-04 15:21:17 +0000

    Added finally method for users next try

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 public class UsingTryCatch {
     public static void main(String[] args) {
         try {
+            //System.out.println("Please enter a number between 0 - 9 :");
             System.out.println("Please enter a number between 0 - 9 :");
             Scanner myscan = new Scanner(System.in);
             int myNum = myscan.nextInt();
@@ -13,7 +14,17 @@ public class UsingTryCatch {
                 System.out.println("Not a valid number");
             }
         } catch (InputMismatchException e) {
-            System.out.println("Please enter a valid numerical value.");
+            System.out.println("Error, you must enter a valid numerical value.");
+        }
+          finally{
+            System.out.println("Please try again.");
+            Scanner myscan = new Scanner(System.in);
+            int myNum = myscan.nextInt();
+            if (myNum < +9 && myNum > 0) {
+                System.out.println("You entered :" + myNum);
+            } else {
+                System.out.println("Not a valid number");
+            }
         }
     }
 }
